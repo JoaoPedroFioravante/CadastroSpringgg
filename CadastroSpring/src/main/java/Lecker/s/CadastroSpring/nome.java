30980a20959ed4366a3d4b94c7df134a145d3007
JoaoPedroFioravante <joaogalleni1376@gmail.com>
2025-03-10 14:55:44 -0300

    #2 issue concluida

--- CadastroSpring/src/main/java/Lecker/s/CadastroSpring/nome.java
+++
@@ -1,7 +0,0 @@
-package Lecker.s.CadastroSpring;
-
-public class nome {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ CadastroSpring/src/main/java/User.java
@@ -0,0 +1,46 @@
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cadastro")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long Id;
+    private String nome;
+    private String eMail;
+    private int idade;
+
+
+    public User() {
+    }
+
+    public User(String nome, String eMail, int idade) {
+        this.nome = nome;
+        this.eMail = eMail;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
